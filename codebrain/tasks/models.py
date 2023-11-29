975903f3fd1a16384bf554c9ea31544638f7cad9
katsuyorii <katsuyoriiii@gmail.com>
2023-11-29 14:56:18 +0300

    add models

--- codebrain/tasks/models.py
+++ codebrain/tasks/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=100)
+    slug = models.SlugField(verbose_name='Слаг', max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Complexity(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=100)
+    slug = models.SlugField(verbose_name='Слаг', max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Сложность'
+        verbose_name_plural = 'Сложности'
+
+
+class ProgLang(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=100)
+    slug = models.SlugField(verbose_name='Слаг', max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Язык программиирования'
+
+
+class Task(models.Model):
+    title = models.CharField(verbose_name='Заголовок задачки', max_length=255)
+    description = models.TextField(verbose_name='Описание задачки')
+    complexity = models.ForeignKey(verbose_name='Сложность', to=Complexity, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(verbose_name='Теги', to=Tag)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задачка'
+        verbose_name_plural = 'Задачки'
+
+
+class Solutiuon(models.Model):
+    task = models.ForeignKey(verbose_name='Задача', to=Task, on_delete=models.CASCADE)
+    prog_lang = models.ForeignKey(verbose_name='Язык программирования', to=ProgLang, on_delete=models.CASCADE)
+    content = models.TextField(verbose_name='Решение задачки')
+
+    def __str__(self):
+        return f'{self.prog_lang.name} | {self.task.title}'
+
+    class Meta:
+        verbose_name = 'Решение'
+        verbose_name_plural = 'Решения'
--- codebrain/users/models.py
+++ codebrain/users/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(verbose_name='Изображение', upload_to='profile-avatars', default='default.png')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
