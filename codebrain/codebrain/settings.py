55e435fa3fb687c9d602b60dc00a34a619c33d93
katsuyorii <katsuyoriiii@gmail.com>
2023-11-30 18:52:27 +0300

    add logout logic

--- codebrain/codebrain/settings.py
+++ codebrain/codebrain/settings.py
@@ -154,3 +154,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 LOGIN_URL = reverse_lazy('index')
+LOGOUT_REDIRECT_URL = reverse_lazy('index')
--- codebrain/users/urls.py
+++ codebrain/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import LoginUserView, ProfileUserView, RegistrationUserView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('profile/', login_required(ProfileUserView.as_view()), name='profile'),
     path('registration/', RegistrationUserView.as_view(), name='registration'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
